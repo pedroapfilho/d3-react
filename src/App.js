791c6141e7c745d107bacfffe6108a4408ba87e1
Pedro Filho <pedro@filho.me>
2021-03-23 07:24:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = [
+  { name: "A", value: 100 },
+  { name: "B", value: 200 },
+  { name: "C", value: 500 },
+  { name: "D", value: 150 },
+  { name: "E", value: 300 },
+];
+
+const margin = { top: 30, right: 0, bottom: 30, left: 40 };
+
+const height = 250;
+
+const width = 300;
+
+const App = () => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.value)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    const x = d3
+      .scaleBand()
+      .domain(d3.range(data.length))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
+
+    const svg = d3
+      .select(ref.current)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    const yAxis = (g) =>
+      g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y).ticks(null, data.format))
+        .call((g) => g.select(".domain").remove())
+        .call((g) =>
+          g
+            .append("text")
+            .attr("x", -margin.left)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text(data.y)
+        );
+
+    const xAxis = (g) =>
+      g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+        d3
+          .axisBottom(x)
+          .tickFormat((i) => data[i].name)
+          .tickSizeOuter(0)
+      );
+
+    svg.append("g").call(xAxis);
+
+    svg.append("g").call(yAxis);
+
+    svg
+      .append("g")
+      .selectAll("rect")
+      .data(data)
+      .join(
+        (enter) => enter.append("rect"),
+        (update) => update,
+        (exit) => exit.remove()
+      )
+      .attr("x", (d, i) => x(i))
+      .attr("y", (d) => y(d.value))
+      .attr("height", (d) => y(0) - y(d.value))
+      .attr("width", x.bandwidth());
+  }, []);
+
+  return <div ref={ref} />;
+};
 
 export default App;
